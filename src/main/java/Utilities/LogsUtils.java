532cbfdf5e31830c349a0a5686f5cf0787524c1d
Marwa <marwaebrahim703608@gmail.com>
2025-08-27 17:23:56 +0300

    intial commit

---
+++ src/main/java/Utilities/LogsUtils.java
@@ -0,0 +1,21 @@
+package Utilities;
+
+//import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+public class LogsUtils {
+    public static String LOGS_PATH = "test-outputs/Logs";
+
+    public static void trace(String message) {
+        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
+                .trace(message);
+    }
+
+    public static void info(String message) {
+        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
+                .info(message);
+    }
+    public static void error(String message) {
+        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
+                .error(message);
+    }
+}
